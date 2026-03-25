c49d5e6813f5791a2fe304d39bd975b5fb1869ba
João Fernandes <112496094+a55u@users.noreply.github.com>
2026-03-25 09:44:43 -0300

    Add files via upload

---
+++ BinarySearchThree.java
@@ -0,0 +1,96 @@
+
+public class BinarySearchThree {
+	
+	BinarySearchThree right;
+	BinarySearchThree left;
+	int key;
+	
+	public BinarySearchThree(int key) {
+		this.right = null;
+		this.left = null;
+		this.key = key;
+	}
+	
+	
+	int getkey() {
+		return this.key;
+	}
+	
+	boolean search(int searchValue) {
+		if(this.key == searchValue)
+			return true;
+		
+		if(searchValue < this.key) {
+			if(this.left == null)
+				return false;
+			
+			return this.left.search(searchValue);
+		}else {
+			if(this.left == null) 
+				return false;
+			
+			return this.right.search(searchValue);
+		}
+	}
+	
+	
+	boolean insert(int value) {
+		if(value < this.key) {
+			if(this.left == null) {
+				this.left = new BinarySearchThree(value);
+				return true;
+				
+			}else 
+				return this.left.insert(value);
+								
+	}else {
+		if(this.right == null) {
+			this.right = new BinarySearchThree(value);
+			return true;
+		}else {
+			return this.right.insert(value);
+		}
+	}///fim condicional else
+		
+		}///fim insert
+
+	
+	void preOrder() {
+		
+		System.out.print(this.key + " ");
+		
+		if(this.left != null) 
+			this.left.preOrder();
+		
+		
+		if(this.right != null)
+			this.right.preOrder();
+		
+	}
+		
+	
+	void inOrder(){
+		if(this.left != null) 
+			this.left.inOrder();
+		
+		System.out.print(this.key + " ");
+		
+		if(this.right != null) 
+			this.right.inOrder();
+		
+	}
+	
+	void postOrder() {
+		if(this.left != null)
+			this.left.postOrder();
+		
+		if(this.right != null)
+			this.right.postOrder();
+		
+	}
+
+	
+}
+	
+
+
